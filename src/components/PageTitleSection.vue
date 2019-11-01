818d74a477f180e81e0a06be5b34f69244970f79
archx3 <georgeranch31@gmail.com>
2019-11-01 10:28:06 +0000

    chore: complete project set up

---
+++ src/components/PageTitleSection.vue
@@ -0,0 +1,51 @@
+<template>
+ <section class="page-title-section overlay" :style="{paddingTop: paddingTop + 'px'}"
+          v-lazy-background="{
+        blur : true,
+        lowResSrc : `${background}?q=3`,
+        highResSrc : `${background}`,
+        }">
+  <div class="container">
+   <div class="row">
+    <div class="col-md-8">
+     <ul class="list-inline custom-breadcrumb">
+      <li class="list-inline-item">
+       <router-link class="h2 text-primary font-secondary" :to="url">{{pageTitle}}</router-link>
+      </li>
+      <li v-if="pageSubTitle" class="list-inline-item text-white h3 font-secondary"><i class="ti-angle-right"></i></li>
+      <li v-if="pageSubTitle" class="list-inline-item text-white h3 font-secondary">{{pageSubTitle}}</li>
+     </ul>
+     <p v-if="pageDescription" class="text-lighten">{{pageDescription}}</p>
+    </div>
+   </div>
+  </div>
+ </section>
+</template>
+<script>
+export default {
+  name: 'page-title-section',
+  props : {
+    url : {
+      // default: this.$route
+    },
+    background : {
+      default : 'https://primaca.sirv.com/Images/about/about-page-n.png'
+    },
+    pageTitle : {},
+    pageSubTitle : {},
+    pageDescription : {
+      default : null
+    },
+    paddingTop : {
+      default : 200
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+ .page-title-section{
+  background-repeat: no-repeat;
+  -webkit-background-size: cover;
+  background-size: cover;
+ }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,61 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue       from 'vue'
+import App       from './App.vue'
+import store     from './store'
+import VueRouter from 'vue-router'
+import Routes    from './router'
 
+import './assets/plugins/bootstrap/bootstrap.min.css'
+import './assets/plugins/slick/slick.css'
+import './assets/plugins/themify-icons/themify-icons.css'
+import './assets/plugins/animate/animate.css'
+import './assets/plugins/aos/aos.css'
+import './assets/plugins/venobox/venobox.css'
+import './assets/styles/scss/style.scss'
+
+import HeaderSection from './components/HeaderSection'
+import PageTitleSection from './components/PageTitleSection'
+import FooterSection from './components/FooterSection'
+import Loader        from "./components/Loader";
+import LazyImage        from "./components/LazyImage";
+import LazyBackground        from "./directives/LazyBackground";
+
+// custom directives
+Vue.directive('lazy-background', LazyBackground);
+Vue.directive('background', function (el, binding) {
+  if (typeof binding.value === 'string') {
+    el.style.backgroundImage =  `url(${binding.value})`;
+  } else if (typeof binding.value === 'object') {
+    for (let property in binding.value) {
+      if (binding.value.hasOwnProperty(property)) {
+        if (property === 'backgroundImage' || property ===  'background-image')
+        {
+          el.style.backgroundImage =  `url(${binding.value})`;
+        }
+      }
+    }
+  }
+});
+
+export const bus = new Vue()
 Vue.config.productionTip = false
+
+const router = new VueRouter({
+  routes: Routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
+  // mode  : 'history'
+})
+Vue.use(VueRouter)
 
 new Vue({
   router,
